d35f3f158f472da6a3f7dea299a91b5da315d705
ab19092002 <128353578+ab19092002@users.noreply.github.com>
2023-04-10 13:43:39 +0700

    Create result.php

---
+++ result.php
@@ -0,0 +1,116 @@
+
+
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<head>
+<title>Free Home Shoppe Website Template | News :: w3layouts</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script> 
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<style type="test/css">
+
+</style>
+</head>
+<body>
+<?php require("conncet_1.php")
+	
+	?>
+  <div class="wrap">
+	<div class="header">
+		<div class="headertop_desc" style="background-color:#0099CC" >
+			<div class="call">
+			<p><span>CUSTOMER SUPPORT</span> Hotline <span class="number"> 1900 1508 </span></span></p>
+			</div>
+			<div class="account_desc">
+				<ul>
+				<li><a href="index.php" style="background-color:#003333;">Home</a></li>
+					<li><a href="login.php" style="background-color:#003333;">Login</a></li>
+					
+			
+				</ul>
+				<style>
+				li {
+					font-size:28px;
+					
+				}
+			</style>
+			</div>
+			<div class="clear"></div>
+	</div>
+		<div class="header_top" style=background-color:#FF9999;>
+		<div class="center" >
+			<video width="400px" height="200px" playsinline autoplay muted loop>
+			<source src="images/DONG.mp4" type="video/mp4" >
+ 			</video>
+			 <style>
+				.center {
+    			 margin: 0 auto;
+				 width: 400px;
+   		 		display: block
+				}
+			</style>
+		
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<style>
+body{
+			background-color:#339999;
+		}
+	</style>
+	<?php session_start();
+		$user = $_SESSION['suser']; 
+		$name = $_SESSION['sname'];
+		echo "Welcome: " .$user." </br>". " </br> Full Name: ".$name; 
+	?>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>	</div>
+	</div>	 
+
+</body>
+</html>
+<br/>
+			 <br/>
+			 
+
+</body>
+</html>
+
+
+ 
+    </div>
+ 
+        <div class="copy_right">
+		<p>&copy; 2013 home_shoppe. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+		   </div>
+    </div>
+   <script type="text/javascript">
+		$(document).ready(function() {			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	</script>
+    <a href="#" id="toTop"><span id="toTopHover"> </span></a>
+</body>
+</html>
+
